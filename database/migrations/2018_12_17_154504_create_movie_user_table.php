c621844efe76ebb2b12d030f57331f8a45d4e1b1
lanzorg <anonymous@example.com>
2018-12-18 10:04:54 +0100

    Fix relations in models and use factories

---
+++ database/migrations/2018_12_17_154504_create_movie_user_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMovieUserTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('movie_user', function(Blueprint $table) {
+			$table->integer('movie_id')->unsigned()->index();
+			$table->integer('user_id')->unsigned()->index();
+			$table->boolean('is_watched')->default(false);
+			$table->boolean('is_favorited')->default(false);
+			$table->primary(['movie_id', 'user_id']);
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('movie_user');
+	}
+}
