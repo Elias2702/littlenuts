da6f7e41d808b3629bbf8bb56383cf11f6aed029
benestruv <benedicte.struvay@gmail.com>
2019-01-03 16:47:09 +0100

    Ajout du formulaire d'ajout d'un nouvel acteur non terminé

---
+++ resources/views/create_actor.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="cardContainer">
+    
+    <create-actor></create-actor>
+
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,33 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
+// GUEST -------------------------------------------------------------------- //
+
+// Homepage
+Route::get('/', 'PublicController@index')->name('index');
+
+// Actors
+Route::get('/actors', 'PublicController@actors')->name('actors.all');
+Route::get('/actors/{id}', 'PublicController@actor')->name('actors.one');
+
+// Movies
+Route::get('/movies', 'PublicController@movies')->name('movies.all');
+Route::get('/movies/{id}', 'PublicController@movie')->name('movies.one');
+
+// AUTH --------------------------------------------------------------------- //
+
+// Authentication
+Auth::routes();
+
+// Dashboard
+Route::get('/home/{action?}', 'HomeController@index')->name('home');
+
+// Others
+Route::get('/user_settings', 'SettingsController@settings');
+Route::patch('/settings', 'SettingsController@edit');
+
+// Ajout Bene
+Route::get('/create_actor', function () {
+    return view('create_actor');
 });
 
-
-Route::resource('movie', 'MovieController');
-Route::resource('actor', 'ActorController');
-Route::resource('list', 'ListController');
-Route::resource('user', 'UserController');
-
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/user_settings', 'SettingsController@settings'); // Afficher la vue profil
-
-Route::patch('/settings', 'SettingsController@edit'); // Editer son profil
-
-Route::get('/movies', 'MovieController@showMovies')->name('show_movies'); // Afficher la liste de film au complet
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/moviecard/{id}', 'MovieController@showMovie')->name('show_movie'); // Afficher un film en particulier
-
-Route::any('search', 'MovieController@searchMovies')->name('search_movies'); // Fonction de recherche de film
-
-/* AJOUT BENEDICTE */
-// Actor List
-Route::get('/actors', function() {
-    $actors = \App\Actor::all();
-    return view('actors', compact('actors'));})->name('actors');
-
-// Pour la page d'un acteur particulier à partir de son id
-Route::get('/actors/{id}', function ($id) {
-    $actor = \App\Actor::find($id);
-    return view('actor', compact('actor'));})->name('show_actor');
-
-// Pour la liste des films
-Route::get('/movies', function() {
-    $movies = \App\Movie::all();
-    return view('movies', compact('movies'));
-})->name('movies');
-
-//Route::get('/actors', 'ActorsList')->name('actors');
+Route::post('/addactors', 'AddActorController@store');
