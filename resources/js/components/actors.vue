9a1b13958778d8688e91429bb6962bfb4fe1f4d5
benestruv <benedicte.struvay@gmail.com>
2019-01-08 10:20:29 +0100

    suppression des fichiers addActor

---
+++ resources/js/components/actors.vue
@@ -0,0 +1,22 @@
+<template>
+        <div v-for "actor in actors">
+            {{actors.firstname}}
+        </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                actor:'',
+                actors: [
+                    {firstname: 'Brad'},
+                    {firstname: 'Angelina'},
+                ]
+            }
+        },
+        methods: {
+
+        }
+    }
+
