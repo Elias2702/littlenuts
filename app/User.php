5baeeb62aa3ef5f855bf39df03e42134138437d1
Elias <elias.djenidi@gmail.com>
2019-01-09 16:22:09 +0100

    fixed User model for new users columns, add a few routes and controller methods, start the /catalogue vue component, get stuck with importing data from DB and use them in the blade

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'firstname', 'lastname', 'email', 'password',
+        'firstname', 'lastname', 'birthday', 'email', 'is_premium', 'referrer_id', 'password',
     ];
 
     /**
