d1ccc4f0b6e157d154bba70ddd799e42cda14e20
lanzorg <anonymous@example.com>
2019-01-18 11:36:18 +0100

    Fix it once again

--- resources/js/components/SearchResults.vue
+++ resources/js/components/SearchResults.vue
@@ -5,11 +5,7 @@
 <script>
 import axios from "axios";
 export default {
-<<<<<<< HEAD
   name: "SearchResults",
-=======
-  name: "Search",
->>>>>>> 6d37aa0cbc9af0bfb585d3f96c8376c1ee8525fe
   data() {
     return {
       keywords: null,
